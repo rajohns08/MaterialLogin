8f294c9f222baafe80fd210da387b4f9b75c1f2d
rajohns <rajohns@bcbsms.com>
2015-02-03 15:56:21 -0600

    updating action bar color

--- app/src/main/java/com/example/rajohns/materiallogin/MainActivity.java
+++ app/src/main/java/com/example/rajohns/materiallogin/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.rajohns.materiallogin;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.transition.Slide;
+import android.transition.Transition;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import com.gc.materialdesign.views.ButtonRectangle;
 import com.pnikosis.materialishprogress.ProgressWheel;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
     ProgressWheel loadingIndicator;
     ButtonRectangle loginButton;
 
@@ -28,6 +34,23 @@ public class MainActivity extends ActionBarActivity {
 
     public void loginTapped(View v) {
         if (loadingIndicator.getVisibility() == View.GONE) {
+            showLoading(true);
+        }
+        else {
+            showLoading(false);
+        }
+
+        startActivity(new Intent(this, PostLogin.class));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        showLoading(false);
+    }
+
+    public void showLoading(boolean shouldShowLoading) {
+        if (shouldShowLoading) {
             loadingIndicator.setVisibility(View.VISIBLE);
             loginButton.setText("");
         }
---
+++ app/src/main/java/com/example/rajohns/materiallogin/PostLogin.java
@@ -0,0 +1,17 @@
+package com.example.rajohns.materiallogin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by rajohns on 2/3/15.
+ *
+ */
+public class PostLogin extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post_login);
+    }
+}
